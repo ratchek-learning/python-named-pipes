ae70ed6fa579595b4e5b052e35c47dadd9e9a346
ratchek <git@ratchek.com>
2022-08-12 16:49:20 -0400

    Create basic server that listens on a named pipe and echoes what was sent

---
+++ listener.py
@@ -0,0 +1,19 @@
+import os 
+
+my_pipe = "da_pipe"
+
+try:
+    os.mkfifo(my_pipe)
+except FileExistsError:
+    print("pipe already exists, continuing")
+
+def start_server():
+    while True:
+        with open(my_pipe) as pipe:
+            for line in pipe:
+                if line == "goodbye\n":
+                    return
+                print(line)
+start_server()
+print("exiting. Goodbye")
+
